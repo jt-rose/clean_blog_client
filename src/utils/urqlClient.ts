4aa436419223632c8fc63fea500955327745efd7
Jeffrey Rose <jtr219@outlook.com>
2021-12-30 23:56:31 -0500

    update urql client to store ssr settings

--- src/utils/urqlClient.ts
+++ src/utils/urqlClient.ts
@@ -1,5 +1,6 @@
-import { createClient } from "urql";
+import { withUrqlClient } from "next-urql";
 
-export const client = createClient({
+export const withUrqlSSR = withUrqlClient((_ssrExchange, _ctx) => ({
+  // ...add your Client options here
   url: "http://localhost:8080/query",
-});
+}));
