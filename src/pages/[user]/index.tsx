c4cd3b276a47ca06853e3c3e697daa366118169c
Jeffrey Rose <jtr219@outlook.com>
2022-01-02 00:29:35 -0500

    set up basic test query using gql codegen

--- src/pages/[user]/index.tsx
+++ src/pages/[user]/index.tsx
@@ -1,10 +1,40 @@
 import { useRouter } from "next/router";
+import { useGetUserWithPostsQuery } from "../../generated/graphql";
+import { client } from "../../utils/gqlClient";
 
+/*
+export async function getStaticProps() {
+    const u = await useGetUserWithPostsQuery(client, { username: "jtr"})
+    u.data?.getUserByUsername?.username
+   return { props: { u } }
+  }
+*/
 const User = () => {
   const router = useRouter();
   const { user } = router.query;
+  const { data, error, isLoading } = useGetUserWithPostsQuery(client, {
+    username: "jtr",
+  });
 
-  return <p>User: {user}</p>;
+  /*const username = typeof user === "string" ? user : "";
+  //const {} = useQuery('', )
+  const [result] = useGetUserWithPostsQuery({
+    variables: { username },
+    context: { fetchOptions: { credentials: "include" } },
+  });
+  const { data, fetching, error } = result;
+  if (error) {
+    console.log("error", error);
+    return <p>Error: {error.message}</p>;
+  }
+  if (fetching) {
+    return <p>...loading</p>;
+  }
+
+  if (!data || !data.getUserByUsername) {
+    return <p>Error: No data returned!</p>;
+  }*/
+  return <p>User: {data?.getUserByUsername?.username}</p>;
 };
 
 export default User;
