95c05ecc2766f1d8bf93eff23ca69bca9bf7e7ba
Jeffrey Rose <jtr219@outlook.com>
2022-01-01 23:56:29 -0500

    remove urql

--- src/utils/urqlClient.ts
+++
@@ -1,6 +0,0 @@
-import { withUrqlClient } from "next-urql";
-
-export const withUrqlSSR = withUrqlClient((_ssrExchange, _ctx) => ({
-  // ...add your Client options here
-  url: "http://localhost:8080/query",
-}));
