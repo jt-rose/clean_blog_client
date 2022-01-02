a1935a9bab23abb4ad390be54836d6aeb790205f
Jeffrey Rose <jtr219@outlook.com>
2022-01-01 23:57:12 -0500

    set up root-level react-query component wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,24 @@ import "../styles/styles.scss";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Footer } from "../components/Footer";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // generate top-level react-query client
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+
       <Footer />
     </>
   );
