55624f418c766e26b429ad446e4404d0811d0888
Jeffrey Rose <jtr219@outlook.com>
2022-01-06 02:45:52 -0500

    set up basic ssr for post page

--- src/pages/[user]/[postID].tsx
+++ src/pages/[user]/[postID].tsx
@@ -1,29 +1,59 @@
-import { useRouter } from "next/router";
-import { posts } from "../../data/posts";
+import { GetServerSideProps } from "next";
+//import { useRouter } from "next/router";
+import { getSdk, GetPostQuery } from "../../generated/graphql-sdk";
+import { client } from "../../utils/gqlClient";
 import { Post } from "../post";
 
-const PostWithData = () => {
-  const router = useRouter();
-  const { postID } = router.query;
-  if (!postID) {
-    return <p>No such entry found!</p>;
-  }
-  const postIDInt = Array.isArray(postID)
-    ? parseInt(postID[0])
-    : parseInt(postID);
-  if (postIDInt === NaN) {
-    return <p>Incorrect post id - please use only a number!</p>;
-  }
+interface PostResponse {
+  post: GetPostQuery | null;
+  error: string | null;
+}
 
-  // simulate looking up in a database
-  const post = posts.find((p) => p.postID === postIDInt);
-  if (!post) {
+export const getServerSideProps: GetServerSideProps = async (
+  context
+): Promise<{ props: PostResponse }> => {
+  try {
+    const { postID } = context.query;
+    const postIDInt = typeof postID === "string" ? parseInt(postID) : 0;
+    if (postIDInt === NaN) {
+      return {
+        props: { post: null, error: "post_id must be a number" },
+      };
+    }
+    const post = await getSdk(client).GetPost({ post_id: postIDInt });
+
+    return { props: { post, error: null } };
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        props: { post: null, error: e.message.replace(/\:.+/, "") },
+      };
+    } else {
+      return {
+        props: {
+          post: null,
+          error: "internal server error",
+        },
+      };
+    }
+  }
+};
+
+const PostWithData = (props: PostResponse) => {
+  console.log(props.post);
+  //const router = useRouter();
+  //const { user } = router.query;
+
+  if (props.error) {
+    return <p>Error: {props.error}</p>;
+  }
+  if (!props.post) {
     return <p>No such post found!</p>;
   }
 
   return (
     <Post>
-      <p>post: {post.title}</p>
+      <p>post: {props.post.getPost?.title}</p>
     </Post>
   );
 };
